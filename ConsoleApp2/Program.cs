20f46be6316a836fcd1ec4531b9bed254c75469f
brunocastroibarburu94 <57188560+brunocastroibarburu94@users.noreply.github.com>
2024-05-29 22:16:32 +0100

    first console projects

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Hello, World!");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine} Hello, {name} on {currentDate:d}");
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # demo-aspnet
 This is a repo for demo to try out ASP.NET development
+
+
+```shell
+dotnet run --project ./ConsoleApp1
+dotnet run --project ./ConsoleApp2
+```
