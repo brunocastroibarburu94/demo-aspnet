49db7730e24289b97ff9787a76e4d0b8e369152e
brunocastroibarburu94 <57188560+brunocastroibarburu94@users.noreply.github.com>
2024-06-01 15:03:28 +0100

    fixed warning message for nullable input of Weather Forecast

--- FoodOrdering/Program.cs
+++ FoodOrdering/Program.cs
@@ -42,8 +42,9 @@ app.MapGet("/weatherforecast", () =>
 .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+#nullable enable
+record WeatherForecast(DateOnly Date, int TemperatureC,string? Summary)
+#nullable disable
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
