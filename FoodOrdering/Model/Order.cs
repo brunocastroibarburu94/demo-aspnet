19ce02d0a5d3b66eebaeb9d1e81ed4befd8bc496
brunocastroibarburu94 <57188560+brunocastroibarburu94@users.noreply.github.com>
2024-06-01 12:11:50 +0100

    added models

---
+++ FoodOrdering/Model/Order.cs
@@ -0,0 +1,18 @@
+public class Order
+{
+    public int Id { get; set; }
+    public int TableNumber { get; set; }
+    public int FoodItemId { get; set; }
+    public FoodItem FoodItem { get; set; }
+    public DateTimeOffset OrderDate { get; set; }
+    public OrderState OrderState { get; set; }
+}
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum OrderState
+{
+    Ordered,
+    Preparing,
+    AwaitingDelivery,
+    Completed
+}
